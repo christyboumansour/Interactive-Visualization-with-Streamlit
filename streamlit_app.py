db597c75c107734766b887e0b80b7e99f7aebb69
christy bou mansour <christyboumansour@gmail.com>
2022-09-23 18:37:52 +0300

    added an about section

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,32 +9,42 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-df=pd.read_csv("COVID-19_in_bd.csv")
+df=pd.read_csv("C:/Users/chris/OneDrive/Desktop/streamlitp/COVID-19_in_bd.csv")
 
-
+#theming
 primaryColor="#F63366"
 backgroundColor="#FFFFFF"
 secondaryBackgroundColor="#F0F2F6"
 textColor="#262730"
 font="sans serif"
 
+#put a title to the webpage
 st.title('COVID-19 in Bangladesh between March-April 2020')
 
+#write text
+txt = st.text_area('About:', '''
+    As the COVID-19 virus made its way worldwide, an opportunity to stop the spread of the virus during its early stages was missed.
+    Let's take a look at how the virus progressed in its early stages during the months of March and April 2020 in Bangladesh and draw some conclusions!'
+    ''')
+
+
+#interaction 1 (button)
 if st.button('Start exploring'):
     st.write('Lets go!')
 
+#pasting original python code form Jupyter into spyder
 st.header("Line Graph")
 fig = px.line(df, x="Date", y="Deaths",title='COVID-19 deaths cases in Bangladesh, between March-April 2020')
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.header("Scatter Plot")
+st.header("3D Scatter Plot")
 fig2 = px.scatter_3d(df, x='Date', y='Recovered', z='Confirmed',
               color='Deaths', title='Variation of Covid 19 confirmed, recovered and deaths cases in Bangladesh (March-April 2020)')
 
 st.plotly_chart(fig2, use_container_width=True)
 
-st.header("3D Scatter Plot")
+st.header("Scatter Plot")
 fig3 = px.scatter(df, x="Date", y="Confirmed",
 	         size="Deaths", color="Deaths", size_max=45, title='Increase of COVID-19 confirmed and deaths cases in Bangladesh, between March and April 2020')
 st.plotly_chart(fig3)
@@ -51,15 +61,15 @@ fig5 = px.scatter(df, x="Confirmed", y="Recovered", animation_frame="Date",
 fig["layout"].pop("updatemenus")
 st.plotly_chart(fig5)
 
-
+#interaction 2: creating sidebar with streamlit allowing us to choose the desired visualization 
 st.sidebar.title('Outline:')
 st.sidebar.markdown("[Line Graph](#line-graph)")
-st.sidebar.markdown('[Scatter Plot](#scatter-plot)')
 st.sidebar.markdown('[3D Scatter Plot](#3d-scatter-plot)')
+st.sidebar.markdown('[Scatter Plot](#scatter-plot)')
 st.sidebar.markdown('[Histogram](#histogram)')
 st.sidebar.markdown('[Scatter with slider](#scatter-with-slider)')
 
-
+#interaction 3: radio button
 change = st.radio(
     "Do you think COVID-19 cases increased in Bangladesh during the indicated timeframe?",
     ('Yes', 'No'))
